4ba94c938900acdfaec61b694d7702ec79649681
Tanawat Pinthongpan <tanawat@magicboxasia.com>
2022-10-11 20:00:55 +0700

    mock button in login page

--- src/pages/Authentication/Login.test.tsx
+++ src/pages/Authentication/Login.test.tsx
@@ -1,5 +1,4 @@
 import type { RenderResult } from '@testing-library/react'
-import type { ButtonProps } from 'components/Button'
 
 import { act, fireEvent, screen, waitFor } from '@testing-library/react'
 
@@ -8,7 +7,15 @@ import { renderWithProviders } from 'helpers/test'
 import { Login } from './Login'
 
 jest.mock('components/Button', () => ({
-  Button: (props: ButtonProps) => <button {...props} data-testid={props.label || props.children} />,
+  Button: ({ link, ...props }: React.HTMLAttributes<never> & { link: boolean }) => {
+    console.log(link)
+
+    return (
+      <div {...props} data-testid={props.children}>
+        {props.children}
+      </div>
+    )
+  },
 }))
 
 describe('<Login />', () => {
